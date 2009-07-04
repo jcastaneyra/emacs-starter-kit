16508722d829f3481f231e7e45aed3e336ad4c39
Clinton R. Nixon <crnixon@gmail.com>
2009-07-04 13:30:31 -0400

    replaced haml-mode with new version

--- cnixon.el
+++ cnixon.el
@@ -23,6 +23,8 @@
 (autoload 'scala-mode "scala-mode-auto" nil t)
 (add-to-list 'auto-mode-alist '("\\.scala" . scala-mode))
 
+(setq default-tab-width 2)
+
 (server-start)
 
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/cnixon/yasnippet"))
