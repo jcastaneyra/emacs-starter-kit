83658df367c083b423730b508bf055809130963e
Clinton R. Nixon <crnixon@gmail.com>
2009-09-24 21:19:45 -0400

    turn off rhtml-mode for a while

--- cnixon/ruby.el
+++ cnixon/ruby.el
@@ -7,20 +7,20 @@
 
 ;; rhtml
 ;; setup rhtml
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/cnixon/rhtml-mode"))
-(autoload 'rhtml-mode "rhtml-mode" nil t)
-(eval-after-load 'rhtml-mode
-  '(progn
-     (add-hook 'rhtml-mode-hook
-               (lambda () (rinari-launch)))
-     (set-face-background 'erb-face "grey10")
-     (set-face-background 'erb-delim-face "grey15")
-     (auto-fill-mode 0)
-     (yas/minor-mode 1)
-     (define-key rhtml-mode-map (kbd "C-h C-r") 'open-ruby-doc)
-     (define-key rhtml-mode-map (kbd "C-h C-l") 'open-rails-doc)))
+;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/cnixon/rhtml-mode"))
+;; (autoload 'rhtml-mode "rhtml-mode" nil t)
+;; (eval-after-load 'rhtml-mode
+;;   '(progn
+;;      (add-hook 'rhtml-mode-hook
+;;                (lambda () (rinari-launch)))
+;;      (set-face-background 'erb-face "grey10")
+;;      (set-face-background 'erb-delim-face "grey15")
+;;      (auto-fill-mode 0)
+;;      (yas/minor-mode 1)
+;;      (define-key rhtml-mode-map (kbd "C-h C-r") 'open-ruby-doc)
+;;      (define-key rhtml-mode-map (kbd "C-h C-l") 'open-rails-doc)))
 
-(add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.rhtml$" . rhtml-mode))
+;; (add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
+;; (add-to-list 'auto-mode-alist '("\\.rhtml$" . rhtml-mode))
 
 
