99492c91e81b02733317e1f5aa52da4d5eb38588
Clinton R. Nixon <crnixon@gmail.com>
2009-12-07 15:35:16 -0500

    Removed machine-specific files

--- cnixon.el
+++ cnixon.el
@@ -1,25 +1,8 @@
-(setq browse-url-browser-function 'browse-url-default-macosx-browser)
-
 ;; turn on cua-mode
 (cua-mode t)
 
 (require 'textmate)
 (textmate-mode)
-
-;; scala
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/cnixon/scala-mode"))
-(autoload 'scala-mode "scala-mode-auto" nil t)
-(add-to-list 'auto-mode-alist '("\\.scala" . scala-mode))
-
-;; haskell
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/cnixon/haskell-mode"))
-(autoload 'haskell-mode "haskell-site-file" nil t)
-(add-to-list 'auto-mode-alist '("\\.hs" . haskell-mode))
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;; (require 'inf-haskell)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-
 
 (setq default-tab-width 2)
 
@@ -31,10 +14,13 @@
 (yas/load-directory "~/.emacs.d/cnixon/snippets")
 (add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand)
 
-(require 'wombat)
-(color-theme-wombat)
-
 (setq visible-bell nil)
 (column-number-mode t)
 (setq ns-command-modifier 'meta)
 (windmove-default-keybindings 'meta)
+
+;; mac-specific
+(setq browse-url-browser-function 'browse-url-default-macosx-browser)
+(set-default-font "Menlo-12")
+(require 'wombat)
+(color-theme-wombat)
